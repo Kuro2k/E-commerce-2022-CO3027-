8622d757378ad7aec77a7cc5106d7b42df48f745
lamtran29 <lam.tran29@hcmut.edu.vn>
2022-03-21 19:13:03 +0700

    update js for price

--- BTL/9F_E-commerce/public/js/app.js
+++ BTL/9F_E-commerce/public/js/app.js
@@ -1,25 +1,28 @@
-const price = document.getElementsByClassName("price");
-const len = price.length;
-const total = document.getElementsByClassName("total-product-price");
-const qty = document.getElementsByClassName("qty");
-const total_all = document.getElementsByClassName("count-price");
-var total_count = 0;
-
-
-for (let i = 0; i < len; i++){
-    let total_ammount = parseInt(price[i].innerHTML) * qty[i].value;
-    total[i].innerHTML = total_ammount.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
-    total_count += total_ammount;
+function formatPrice() {
+    const price = document.getElementsByClassName("price");
+    const len = price.length;
+    for (let i = 0; i < len; i++){
+        price[i].innerHTML = price[i].innerHTML.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    }
 }
-total_all[0].innerHTML = total_count.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 
-for (let i = 0; i < len; i++){
-    price[i].innerHTML = price[i].innerHTML.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+function unformatPrice(price) {
+    return parseFloat(price) * 1000;
+}
+
+function formatOnePrice(price) {
+    return price.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 }
 
 function calculateProduct() {
-    total_count = 0;
-    for (let i = 0; i < document.getElementsByClassName("price").length; i++){
+    const price = document.getElementsByClassName("price");
+    const len = price.length;
+    const total = document.getElementsByClassName("total-product-price");
+    const qty = document.getElementsByClassName("qty");
+    const total_all = document.getElementsByClassName("count-price");
+    var total_count = 0;
+
+    for (let i = 0; i < len; i++){
         let total_ammount = parseFloat(price[i].innerHTML) * 1000 * qty[i].value;
         total[i].innerHTML = total_ammount.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
         total_count += total_ammount;
@@ -55,3 +58,8 @@ function increaseCartCounter() {
     document.getElementById("cart-product-counter").innerHTML = value;
 }
 
+
+
+
+
+formatPrice();
