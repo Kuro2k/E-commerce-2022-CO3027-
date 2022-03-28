f11c9d6d25955855922d8dd54a14a5639e682896
lamtran29 <lam.tran29@hcmut.edu.vn>
2022-03-28 16:40:17 +0700

    update product and cart

--- BTL/9F_E-commerce/public/js/app.js
+++ BTL/9F_E-commerce/public/js/app.js
@@ -31,26 +31,6 @@ function calculateProduct() {
 
 }
 
-function increaseCount(a, b) {
-    var input = b.previousElementSibling;
-    var value = parseInt(input.value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    input.value = value;
-    calculateProduct();
-}
-    
-function decreaseCount(a, b) {
-    var input = b.nextElementSibling;
-    var value = parseInt(input.value, 10);
-    if (value > 1) {
-        value = isNaN(value) ? 0 : value;
-        value--;
-        input.value = value;
-    }
-    calculateProduct();
-}
-
 
 function increaseCartCounter() {
     var value = parseInt(document.getElementById("cart-product-counter").innerHTML, 10);
