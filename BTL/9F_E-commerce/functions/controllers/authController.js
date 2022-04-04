9fe366f29a8bbfea52241c3846628c8e435278d4
Gia Quang <77892119+Kuro2k@users.noreply.github.com>
2022-04-04 16:56:45 +0700

    update signup

--- BTL/9F_E-commerce/functions/controllers/authController.js
+++ BTL/9F_E-commerce/functions/controllers/authController.js
@@ -3,12 +3,15 @@ const { async } = require("@firebase/util");
 var {auth} = require('../FirebaseAdminConfig');
 var {user_app} = require("../FirebaseUserConfig");
 var firestore = require("firebase/firestore");
+const e = require("express");
+const { json } = require("express");
 
 const createUser = async (req, res) => {
-    const {name,email, phone, password} = req.body;
+    try {
+    const {name,email, phone, psw, re_psw} = JSON.parse(req.body);
     const user = await auth.createUser({
         email: email,
-        password: password,
+        password: psw,
         displayName: name,
         phoneNumber: "+84" + phone
     });
@@ -19,7 +22,10 @@ const createUser = async (req, res) => {
     const db = firestore.getFirestore(user_app);
     const user_col = firestore.collection(db, "User");
     await firestore.addDoc(user_col, user_doc);
-    res.redirect('/');
+    res.status(200).send({result: "Success"})
+} catch (error) {
+    res.status(200).send({error: error.message})
+}
 };
 
 module.exports = createUser;
