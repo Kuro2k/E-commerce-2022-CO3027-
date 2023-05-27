1fb520af33a065763bc9411fa7127c5fce2b13e7
Gia Quang <77892119+Kuro2k@users.noreply.github.com>
2023-05-27 10:53:36 +0700

    Update FirebaseUserConfig.js

--- BTL/9F_E-commerce/functions/FirebaseUserConfig.js
+++ BTL/9F_E-commerce/functions/FirebaseUserConfig.js
@@ -1,13 +1,13 @@
 var firebase = require("firebase/app");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAYOfXw-7HAFfrOjdwWK8eyKpSNT2Qy1Tg",
-    authDomain: "farmnine-6d2d9.firebaseapp.com",
-    projectId: "farmnine-6d2d9",
-    storageBucket: "farmnine-6d2d9.appspot.com",
-    messagingSenderId: "527234496843",
-    appId: "1:527234496843:web:8651706cd64f05f19690ce",
-    measurementId: "G-GB1RK8N62V"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 var user_app=firebase.initializeApp(firebaseConfig);
